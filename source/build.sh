43c1f30d98c63109adcc96f6264ea940dae39652
Steve Russell <41975381+SteveRussell33@users.noreply.github.com>
2025-08-03 10:02:06 +0100

    Update build.sh
    Signed-off-by: Steve Russell <41975381+SteveRussell33@users.noreply.github.com>

--- source/build.sh
+++ source/build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-g++ cppfront.cpp -O3 -pg -std=c++20 -Wa,-mbig-obj -o cppfront
+# g++ cppfront.cpp -O3 -pg -std=c++20 -Wa,-mbig-obj -o cppfront
+g++ cppfront.cpp -O3 -std=c++20 -Wa,-mbig-obj -o cppfront
+cp -f cppfront.exe ../
+../cppfront -vers
